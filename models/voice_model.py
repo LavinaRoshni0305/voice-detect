913e3b6ce20d355696177a4bcade0f9e0b45c59f
Lavina Manoj <150411309+LavinaRoshni0305@users.noreply.github.com>
2025-09-02 23:50:01 +0530

    voice_model.py

--- models/voice_model.py
+++ models/voice_model.py
@@ -2,16 +2,15 @@ from faster_whisper import WhisperModel
 import numpy as np
 import soundfile as sf
 
-# Load the model once
-model_size = "small"  # or "base", "medium" (larger = more accurate)
-model = WhisperModel(model_size)
+# Use tiny English model + CPU + int8
+model = WhisperModel("tiny.en", device="cpu", compute_type="int8")
 
-# Emergency trigger phrases
 trigger_phrases = ["help me luna", "sos", "emergency", "save me"]
 
 def detect_voice(file_path):
     audio, samplerate = sf.read(file_path)
     segments, info = model.transcribe(audio, beam_size=5)
+    
     text = ""
     for segment in segments:
         text += segment.text.lower() + " "
